79e48787d1267d85040e62564a4eefe2544debb0
Aksh <toakshh@gmail.com>
2023-05-19 03:33:00 +0530

    updated config of backend

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks"
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://qkart-eeiz.onrender.com/api/v1`,
 };
 
 function App() {
